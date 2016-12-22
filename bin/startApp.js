ca90b738cc2ebd95b019d4821802cc82e4d4cb4b
Alex Deschamps <alexadeschamps@gmail.com>
2016-12-22 16:48:00 -0500

    adds use strict to the top of all files

--- bin/startApp.js
+++ bin/startApp.js
@@ -1,7 +1,10 @@
+'use strict';
+
 let createServer = require('../lib/server');
 let config = require('config');
 let server = createServer(config);
-let port = process.env.APP_PORT || config.port;
+let port = process.env.APP_PORT || config.app.port;
+
 server.listen(port, () => {
   console.log('server listening on port: ' + port);
 });
